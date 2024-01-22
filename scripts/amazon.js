174f0dbde1ea112eb5bdfca8f0bfb47748ac3f89
nishanth-ksn <ksnishanth05@gmail.com>
2024-01-22 12:04:59 +0530

    generate HYML fofr the order summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML = '';
@@ -59,25 +59,22 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function updtecartQuantity () {
+function updateCartQuantity () {
     let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
 
-            cart.forEach((cartItem) => {
-                cartQuantity += cartItem.quantity;
-            });
-
-            document.querySelector('js-cart-quantity')
-                .innerHTML = cartQuantity;
+    document.querySelector('js-cart-quantity')
+        .innerHTML = cartQuantity;
 }
+
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
 
-            addToCart(productId);
-            updtecartQuantity();
+            updateCartQuantity();
 
-            console.log(cartQuantity)
-            console.log(cart);
         });
     });
