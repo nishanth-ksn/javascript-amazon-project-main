08c592b6d043c35f77be7ee7c8ba96fce9814e2c
nishanth-ksn <ksnishanth05@gmail.com>
2024-01-23 14:16:29 +0530

    save cart in localstorage

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { addToCart, cart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -62,11 +62,12 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity () {
     let cartQuantity = 0;
+
     cart.forEach((cartItem) => {
         cartQuantity += cartItem.quantity;
     });
 
-    document.querySelector('js-cart-quantity')
+    document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
 }
 
@@ -74,7 +75,7 @@ document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
-
+            addToCart(productId);
             updateCartQuantity();
 
         });
